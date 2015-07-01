b38d782726063d50faac6cdbe182a91d22a7f877
Muoi Ly <muoi.ly@gmail.com>
2015-06-30 23:13:25 -0700

    updated test_server.py

--- test_server.py
+++ test_server.py
@@ -3,8 +3,8 @@ import socket
 import pytest
 from multiprocessing import Process
 
-ADDR = ('127.0.0.1', 8080)
-
+ADDR = ('127.0.0.1', 8020)
+_CRLF = b'\r\n'
 
 @pytest.yield_fixture()
 def server_setup():
@@ -19,6 +19,7 @@ def client_setup():
     client = socket.socket(
         socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
     )
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     client.connect(ADDR)
     return client
 
@@ -27,5 +28,30 @@ def test_response_ok(server_setup):
     assert '200 OK' in server.response_ok()
 
 
-def test_response_error(server_setup):
-    assert '500 Internal Server Error' in server.response_error()
+def test_response_error_500(server_setup):
+    valid = []
+    valid.append(b'HTTP/1.1 500 Internal Server Error')
+    valid.append(b'Content-Type: text/html; charset=UTF-8'+ _CRLF)
+    valid = _CRLF.join(valid)
+
+    response = server.response_error(500, 'Internal Server Error')
+    assert valid == response
+
+
+def test_response_error_504(server_setup):
+    valid = []
+    valid.append(b'HTTP/1.1 504 Gateway Timeout')
+    valid.append(b'Content-Type: text/html; charset=UTF-8'+ _CRLF)
+    valid = _CRLF.join(valid)
+    response = server.response_error(504, 'Gateway Timeout')
+    assert valid == response
+
+
+def test_parse_request_valid_request():
+    request = []
+    request.append(b'GET /path/to/URI HTTP/1.1')
+    request.append(b'Host: www.google.com')
+    request = (b'\r\n').join(request)
+    response = server.parse_request(request)
+    assert response == b'/path/to/URI'
+
